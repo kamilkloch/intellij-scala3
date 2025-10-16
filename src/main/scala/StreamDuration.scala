5b0180cbc1123bc3a144701343fd9d7e813ddd21
Kamil Kloch <kamil.kloch@superfund-technologies.com>
2025-10-16 13:15:26 +0200

    Added StreamDuration.

--- src/main/scala/StreamDuration.scala
+++ src/main/scala/StreamDuration.scala
@@ -4,7 +4,7 @@ import scala.concurrent.duration.*
 
 object StreamDuration extends IOApp.Simple {
   override def run: IO[Unit] = {
-    Stream.duration].foreach(_ => IO.sleep(1.second)).compile.drain
+    Stream.duration.foreach(_ => IO.sleep(1.second)).compile.drain
 //    Stream.duration[IO].foreach(_ => IO.sleep(1.second)).compile.drain
   }
 }
